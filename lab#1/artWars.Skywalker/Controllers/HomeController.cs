6eb5b299787c09bf3d719b992c78a4068bb5d545
skdn <skidne@users.noreply.github.com>
2018-02-13 21:33:54 +0200

    firstComm

---
+++ lab#1/artWars.Skywalker/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace artWars.Skywalker.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Battles()
+        {
+            return View();
+        }
+
+        public ActionResult Wars()
+        {
+            return View();
+        }
+
+        public ActionResult Postbellum()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ lab#1/artWars.Skywalker/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="artWars.Skywalker.Global" Language="C#" %>
